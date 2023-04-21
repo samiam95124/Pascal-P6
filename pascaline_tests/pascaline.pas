5a6cca11ecffb6f82a6b96ef727117b02189e053
samiam95124 <samiam@moorecad.com>
2023-04-21 08:40:23 -0700

    Added double escape test.

--- pascaline_tests/pascaline.pas
+++ pascaline_tests/pascaline.pas
@@ -2496,6 +2496,7 @@ begin
    write(ord('\rs'):1, ' ');
    write(ord('\us'):1, ' ');
    writeln(ord('\del'):1, ' ');
+   writeln(ord('\\'):1, ' ');
    writeln('s/b 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 17 18 19 19 20');
    writeln('21 22 23 24 25 26 27 28 29 30 31 127');
 
