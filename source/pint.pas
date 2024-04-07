9db6d725c8e2d992f13201caf10fcb44a57698ff
samiam95124 <samiam@moorecad.com>
2024-04-07 10:54:33 -0700

    Change pint assignexternalbin() as done for assignexternaltext().

--- source/pint.pas
+++ source/pint.pas
@@ -1198,8 +1198,7 @@ begin
   { skip leading spaces }
   while not eolncommand and not eofcommand and (bufcommand = ' ') do getcommand;
   i := 1;
-  while not eolncommand and not eofcommand and
-        (bufcommand in ['a'..'z',  'A'..'Z', '0'..'9', '_']) do begin
+  while not eolncommand and not eofcommand and (bufcommand <> ' ') do begin
     if i = fillen then errorv(FileNameTooLong);
     fne[i] := bufcommand;
     getcommand;
