6463511cd7b17a76cb615f78c39adc5af15f22c2
samiam95124 <samiam@moorecad.com>
2024-04-11 05:15:03 -0700

    Refactored testp6 script for iso7185 host; refactor memory options for pint and pmach; doc updates.
     Changes to be committed:
    	modified:   doc/the_p6_compiler.odt
    
    1. Documentation updates.
    
    	new file:   hosts/iso7185/testp6
    
    1. Refactored testp6 for the iso7185 host.
    
    	modified:   source/pint.pas
    	modified:   source/pmach.pas
    
    1. Changed the macro store options. Now the default is large memory size by
    default, and short memory size by SELF_COMPILE or SHORT_STORE flag. The
    default is also 32/64 bit, or 16 bit on WRDSIZ16 flag.
    
    Notes:
    
    1. Self compile flag is only used for gpc, and is going away. SHORT_STORE flag
    is not the principle means to select reduced storage for running within the
    interpreter(s).

--- source/pint.pas
+++ source/pint.pas
@@ -235,24 +235,20 @@ const
 
       { !!! Need to use the small size memory to self compile, otherwise, by
         definition, pint cannot fit into its own memory. }
-#if !defined(SELF_COMPILE) || defined(BIG_STORE)
+#if defined(SELF_COMPILE) || defined(SHORT_STORE)
+      maxstr     =  2000000;   { maximum size of addressing for program/var }
+      maxtop     =  2000001;   { maximum size of addressing for program/var+1 }
+      maxdef      = 250000;    { maxstr /8 for defined bits }
+#else
 #ifdef WRDSIZ16
       maxstr      = 31999;  { maximum size of addressing for program/var }
       maxtop      = 32000;  { maximum size of addressing for program/var+1 }
       maxdef      = 4000;   { maxstr / 8 for defined bits }
-#elif SHORT_STORE
-      maxstr     =  2000000;   { maximum size of addressing for program/var }
-      maxtop     =  2000001;   { maximum size of addressing for program/var+1 }
-      maxdef      = 250000;    { maxstr /8 for defined bits }
 #else
       maxstr      = 16777215;  { maximum size of addressing for program/var }
       maxtop      = 16777216;  { maximum size of addressing for program/var+1 }
       maxdef      = 2097152;   { maxstr / 8 for defined bits }
 #endif
-#else
-      maxstr     =  2000000;   { maximum size of addressing for program/var }
-      maxtop     =  2000001;   { maximum size of addressing for program/var+1 }
-      maxdef      = 250000;    { maxstr /8 for defined bits }
 #endif
 
       maxdigh     = 6;       { number of digits in hex representation of maxstr }
--- source/pmach.pas
+++ source/pmach.pas
@@ -239,7 +239,11 @@ const
 
       { !!! Need to use the small size memory to self compile, otherwise, by
         definition, pint cannot fit into its own memory. }
-#ifndef SELF_COMPILE
+#if defined(SELF_COMPILE) || defined(SHORT_STORE)
+      maxstr     =  2000000;   { maximum size of addressing for program/var }
+      maxtop     =  2000001;   { maximum size of addressing for program/var+1 }
+      maxdef      = 250000;    { maxstr /8 for defined bits }
+#else
 #ifdef WRDSIZ16
       maxstr      = 31999;  { maximum size of addressing for program/var }
       maxtop      = 32000;  { maximum size of addressing for program/var+1 }
@@ -249,10 +253,6 @@ const
       maxtop      = 16777216;  { maximum size of addressing for program/var+1 }
       maxdef      = 2097152;   { maxstr / 8 for defined bits }
 #endif
-#else
-      maxstr     =  2000000;   { maximum size of addressing for program/var }
-      maxtop     =  2000001;   { maximum size of addressing for program/var+1 }
-      maxdef      = 250000;    { maxstr /8 for defined bits }
 #endif
       
       maxdigh     = 6;       { number of digits in hex representation of maxstr }
