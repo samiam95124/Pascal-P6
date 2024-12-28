298c29c39b224eba62d9f8a7fa819f6fb85c38a1
samiam95124 <samiam@moorecad.com>
2024-12-27 23:50:13 -0800

    Regress; remove option table initialization, which is duplicated.

--- source/AMD64/gcc/pgen.pas
+++ source/AMD64/gcc/pgen.pas
@@ -6084,61 +6084,6 @@ begin (* main *)
   fndpow(maxpow8, 8, octdig);
   fndpow(maxpow2, 2, bindig); bindig := bindig+1; { add sign bit }
 
-  { initialize options tables }
-  opts[1]  := 'a         ';
-  opts[2]  := 'b         ';
-  opts[3]  := 'c         ';
-  opts[4]  := 'd         ';
-  opts[5]  := 'e         ';
-  opts[6]  := 'f         ';
-  opts[7]  := 'g         ';
-  opts[8]  := 'h         ';
-  opts[9]  := 'i         ';
-  opts[10] := 'ee        ';
-  opts[11] := '          ';
-  opts[12] := 'l         ';
-  opts[13] := 'm         ';
-  opts[14] := 'n         ';
-  opts[15] := 'o         ';
-  opts[16] := 'p         ';
-  opts[17] := 'q         ';
-  opts[18] := 'r         ';
-  opts[19] := 's         ';
-  opts[20] := 't         ';
-  opts[21] := 'u         ';
-  opts[22] := 'v         ';
-  opts[23] := 'w         ';
-  opts[24] := 'x         ';
-  opts[25] := 'y         ';
-  opts[26] := 'z         ';
-
-  optsl[1]  := 'debugflt  ';
-  optsl[2]  := 'prtlab    ';
-  optsl[3]  := 'lstcod    ';
-  optsl[4]  := 'chk       ';
-  optsl[5]  := 'machdeck  ';
-  optsl[6]  := 'debugsrc  ';
-  optsl[7]  := 'prtlabdef ';
-  optsl[8]  := 'sourceset ';
-  optsl[9]  := 'varblk    ';
-  optsl[10] := 'experror  ';
-  optsl[11] := 'echoline  ';
-  optsl[12] := 'list      ';
-  optsl[13] := 'breakheap ';
-  optsl[14] := 'recycle   ';
-  optsl[15] := 'chkoverflo';
-  optsl[16] := 'chkreuse  ';
-  optsl[17] := 'chkundef  ';
-  optsl[18] := 'reference ';
-  optsl[19] := 'iso7185   ';
-  optsl[20] := 'prttables ';
-  optsl[21] := 'undestag  ';
-  optsl[22] := 'chkvar    ';
-  optsl[23] := 'debug     ';
-  optsl[24] := 'prtlex    ';
-  optsl[25] := 'prtdisplay';
-  optsl[26] := '          ';
-
   write('P6 Pascal AMD64/gcc 64 bit code generator vs. ', majorver:1, '.', minorver:1);
   if experiment then write('.x');
   writeln;
