0bccfbd076f4609d76667ad39fd0ac0a549c4251
samiam95124 <samiam@moorecad.com>
2025-09-12 23:36:31 -0700

    Add pstring version of listl().

--- libs/source/services_wrapper.asm
+++ libs/source/services_wrapper.asm
@@ -13,12 +13,23 @@
     jmp     1f      # skip stack sequence
 
     .globl  services.list$p_vc_pr$name$0$pvc$size$8$i$alloc$16$i$attr$24$sx$atexec$atarc$atsys$atdir$atloop$$create$56$i$modify$64$i$access$72$i$backup$80$i$user$88$sx$pmread$pmwrite$pmexec$pmdel$pmvis$pmcopy$pmren$$group$120$sx$pmread$pmwrite$pmexec$pmdel$pmvis$pmcopy$pmren$$other$152$sx$pmread$pmwrite$pmexec$pmdel$pmvis$pmcopy$pmren$$next$184$p2$
+    .globl  services.list$p_pvc_pr$name$0$pvc$size$8$i$alloc$16$i$attr$24$sx$atexec$atarc$atsys$atdir$atloop$$create$56$i$modify$64$i$access$72$i$backup$80$i$user$88$sx$pmread$pmwrite$pmexec$pmdel$pmvis$pmcopy$pmren$$group$120$sx$pmread$pmwrite$pmexec$pmdel$pmvis$pmcopy$pmren$$other$152$sx$pmread$pmwrite$pmexec$pmdel$pmvis$pmcopy$pmren$$next$184$p2$
+
 
 services.list$p_vc_pr$name$0$pvc$size$8$i$alloc$16$i$attr$24$sx$atexec$atarc$atsys$atdir$atloop$$create$56$i$modify$64$i$access$72$i$backup$80$i$user$88$sx$pmread$pmwrite$pmexec$pmdel$pmvis$pmcopy$pmren$$group$120$sx$pmread$pmwrite$pmexec$pmdel$pmvis$pmcopy$pmren$$other$152$sx$pmread$pmwrite$pmexec$pmdel$pmvis$pmcopy$pmren$$next$184$p2$:
     pushq   %rdx                # save **fl
     call    pa_listl            # call C routine
-    popq    %rdx                # restore **fl
-    movq    %rdx,%rdi           # move **fl to 1st arg
+    popq    %rdi                # restore **fl to 1st
+    call    cfilelist2pascaline # convert to Pascaline
+    ret
+
+services.list$p_pvc_pr$name$0$pvc$size$8$i$alloc$16$i$attr$24$sx$atexec$atarc$atsys$atdir$atloop$$create$56$i$modify$64$i$access$72$i$backup$80$i$user$88$sx$pmread$pmwrite$pmexec$pmdel$pmvis$pmcopy$pmren$$group$120$sx$pmread$pmwrite$pmexec$pmdel$pmvis$pmcopy$pmren$$other$152$sx$pmread$pmwrite$pmexec$pmdel$pmvis$pmcopy$pmren$$next$184$p2$:
+    movq    %rsi,%rdx           # move **fl to 3rd
+    movq    (%rdi),%rsi         # move string len to 2nd
+    addq    $8,%rdi             # index string data
+    pushq   %rdx                # save **fl
+    call    pa_listl            # call C routine
+    popq    %rdi                # restore **fl to 1st
     call    cfilelist2pascaline # convert to Pascaline
     ret
 
