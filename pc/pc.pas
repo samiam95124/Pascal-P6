acefcee2f55eadb2cc833a31b93400cdce1e18cb
samiam95124 <samiam@moorecad.com>
2025-12-03 23:06:35 -0800

    Build only if module contains code.

--- pc/pc.pas
+++ pc/pc.pas
@@ -1556,7 +1556,7 @@ end;
 
 begin
 
-   if fp^.rebld then begin { this section is to be rebuilt }
+   if fp^.rebld and fp^.code then begin { this section is to be rebuilt }
 
       copy(fns, fp^.name); { make a copy of the name }
       services.brknam(fns, p, n, e); { remove the extention and place .pas }
